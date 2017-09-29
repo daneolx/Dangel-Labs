f203a043b191d823ebf6d165c233b78a8bb2cf52
Daneolx <angelco@hotmail.es>
2017-09-28 21:32:55 -0500

    Update Mailing
    Actualizando correo

--- inicio/php/sendemail.php
+++ inicio/php/sendemail.php
@@ -2,7 +2,7 @@
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name=$_REQUEST['name'];
     $email=$_REQUEST['email'];
-    $to= 'contacto@dangellabs.com'; //change to ur mail address
+    $to= 'angelco@hotmail.es'; //change to ur mail address
     $subject=$_REQUEST['subject'];
     $message =$_REQUEST['message'];     
     $headers = 'MIME-Version: 1.0'."\r\n";
